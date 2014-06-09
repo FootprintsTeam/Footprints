338611a92af796ce31fbd413ff6805a6d85ff759
nhanloukiala <tantrokiniem@gmail.com>
2014-06-10 00:28:27 +0700

    first commit

---
+++ Footprints/DAL/Repository/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Footprints.DAL.Repository
+{
+    public class DbContext
+    {
+        public static Users Users { get { return new Users(); } }
+        public static Comments Comments { get { return new Comments(); } }
+        public static Destinations Destinations { get { return new Destinations(); } }
+        public static Journeys Journeys { get { return new Journeys(); } }
+    }
+}
---
+++ Footprints/DAL/Repository/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Footprints.DAL.Repository
+{
+    class Users
+    {
+    }
+}
