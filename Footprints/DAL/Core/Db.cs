700e0bf4d9cf22eae0ed69b76afe3529a352484d
nhanloukiala <tantrokiniem@gmail.com>
2014-06-10 08:13:04 +0700

    nhan commit

---
+++ Footprints/DAL/Core/Db.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Footprints.DAL.Core
+{
+    public class Db
+    {
+        public string connectionString { get; set; }
+
+        public Db(string conn = null)
+        {
+            
+        }
+    }
+}
