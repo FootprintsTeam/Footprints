2a4a6ae06b8b36e8c3026512e0b5ba3dc5061ff1
njen <tantrokiniem@gmail.com>
2014-06-11 11:05:05 +0700

    dependency injection test

--- Footprints/App_Start/Bootstrapper.cs
+++ Footprints/App_Start/Bootstrapper.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Web;
 using Autofac;
 using Neo4jClient;
+using Footprints.DAL.Core;
+using Footprints.DAL.Infrastructure;
+using Footprints.DAL.Repository;
 
 namespace Footprints.App_Start
 {
     public class Bootstrapper
     {
-        public static void Run()
-        {
-            SetAutofacContainer();
-        }
-
-        public static void SetAutofacContainer()
+        public static IContainer SetAutofacContainer()
         {
             var builder = new ContainerBuilder();
             builder.Register<IGraphClient>(context =>
@@ -23,6 +21,13 @@ namespace Footprints.App_Start
                 graphClient.Connect();
                 return graphClient;
             }).SingleInstance();
+            builder.RegisterType<ICommentRepository>().As<Comments>();
+            builder.RegisterType<IJourneyRepository>().As<Journeys>();
+            builder.RegisterType<IDestinationRepository>().As<IDestinationRepository>();
+            builder.RegisterType<IUserRepository>().As<Users>();
+            builder.RegisterType<IDbContext>().As<DbContext>();
+            var container = builder.Build();
+            return container;
         }
     }
 }
--- Footprints/Global.asax.cs
+++ Footprints/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Footprints.App_Start;
 
 namespace Footprints
 {
@@ -16,6 +17,8 @@ namespace Footprints
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Autofac setup
+            DependencyResolver.SetResolver(Bootstrapper.SetAutofacContainer());
         }
     }
 }
