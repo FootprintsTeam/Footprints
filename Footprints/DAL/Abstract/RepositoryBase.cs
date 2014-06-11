7ea2e7317c16a43425d0a4da9e80677cf0d37755
nhanloukiala <tantrokiniem@gmail.com>
2014-06-11 12:14:21 +0700

    Add fake object library Moq for Unit Test

--- Footprints/DAL/Abstract/RepositoryBase.cs
+++ Footprints/DAL/Abstract/RepositoryBase.cs
@@ -43,17 +43,4 @@ namespace Footprints.DAL.Abstract
             return null;
         }
     }
-
-    public interface IRepository<T> where T : class
-    {
-        void Add(T entity);
-        void Update(T entity);
-        void Delete(T entity);
-        void Delete(Expression<Func<T, bool>> where);
-        T GetById(long id);
-        T GetById(string id);
-        T Get(Expression<Func<T, bool>> where);
-        IEnumerable<T> GetAll();
-        IEnumerable<T> GetMany(Expression<Func<T, bool>> where);
-    }
 }
