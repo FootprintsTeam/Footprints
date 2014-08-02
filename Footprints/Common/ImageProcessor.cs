6ff56dbcad6f5918fdf026928b33bd10e68e38e8
hung-vi <ngochung.vi90@gmail.com>
2014-08-02 12:16:03 +0700

    Upload photo
    Upload photo

---
+++ Footprints/Common/ImageProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Web.UI.WebControls;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Footprints.Common
+{
+    public class ImageProcessor
+    {
+        public static string UploadPhotoWithThumb(Guid UserID, Guid AlbumID, Stream imageStream)
+        {
+            //Upload image to s3
+            string uniqueFileName = Guid.NewGuid().ToString().Replace('.', '_').Replace('-', '_') + ".jpg";
+            string photoPath = UserID.ToString() + "/" + AlbumID.ToString() + "/" + uniqueFileName;
+            string photoThumbPath = UserID.ToString() + "/" + AlbumID.ToString() + "/thumbnails/" + uniqueFileName;
+            try
+            {
+                //Generate thumbnails image
+                System.Drawing.Image uploadImage = System.Drawing.Image.FromStream(imageStream);
+                System.Drawing.Image resizedImage = ImageUtil.ResizeThumbnail(uploadImage);
+                MemoryStream ms = new MemoryStream();
+                resizedImage.Save(ms, ImageFormat.Jpeg);
+                //Put thumbnail photo to album
+                UploadPhoto(photoThumbPath, ms);
+                //Put original image file to album
+                UploadPhoto(photoPath, imageStream);
+            }
+            catch (ArgumentException e)
+            {
+                Debug.WriteLine(e.Message);
+                return String.Empty;
+            }
+            return uniqueFileName;
+        }
+
+        public static void DeletePhoto(Guid UserID, Guid AlbumID, string name)
+        {
+            using (IAmazonS3 s3Client = Amazon.AWSClientFactory.CreateAmazonS3Client(Amazon.RegionEndpoint.APSoutheast1))
+            {
+                DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest();
+                deleteObjectsRequest.BucketName = System.Configuration.ConfigurationManager.AppSettings["ImageBucketName"];
+                deleteObjectsRequest.AddKey(UserID.ToString() + "/" + AlbumID.ToString() + "/" + name);
+                DeleteObjectsResponse deleteObjectResponse = s3Client.DeleteObjects(deleteObjectsRequest);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key">absolute path to store image in the bucket, for example, userid/albumid/GUID.jpg</param>
+        /// <param name="imageStream">stream of the uploaded photo</param>
+        /// <param name="client">AWS S3 client</param>
+        public static void UploadPhoto(string key, Stream imageStream)
+        {
+            try
+            {
+                using (IAmazonS3 s3Client = Amazon.AWSClientFactory.CreateAmazonS3Client(Amazon.RegionEndpoint.APSoutheast1))
+                {
+                    PutObjectRequest putObjectRequest = new PutObjectRequest();
+                    putObjectRequest.BucketName = System.Configuration.ConfigurationManager.AppSettings["ImageBucketName"];
+                    putObjectRequest.CannedACL = S3CannedACL.PublicRead;
+                    putObjectRequest.InputStream = imageStream;
+                    putObjectRequest.Key = key;
+                    PutObjectResponse response = s3Client.PutObject(putObjectRequest);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Upload image stream to Amazon s3
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="albumid"></param>
+        /// <param name="imageStream"></param>
+        /// <param name="client"></param>
+        public static string UploadPhoto(Guid UserID, Guid AlbumID, Stream imageStream)
+        {
+            string photoPath = UserID.ToString() + "/" + AlbumID.ToString() + "/" + new Guid(Guid.NewGuid().ToString("N")) + ".jpg";
+            UploadPhoto(photoPath, imageStream);
+            return photoPath;
+        }
+    }
+}
---
+++ Footprints/Common/ImageUtil.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace Footprints.Common
+{
+    public class ImageUtil
+    {
+        const int ALBUM_PHOTO_THUMB_WIDTH = 200;
+        const int ALBUM_PHOTO_THUMB_HEIGH = 200;
+
+        /// <summary>
+        /// Validate image file uploaded
+        /// </summary>
+        /// <param name="fileUpload">uploaded file</param>
+        /// <returns>true: fileUpload is image file</returns>
+        public static bool IsValidImage(Stream imageStream)
+        {
+            try
+            {
+                using (var bitmap = new System.Drawing.Bitmap(imageStream))
+                {
+                    if (bitmap.Size.IsEmpty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Resizes and rotates an image, keeping the original aspect ratio. Does not dispose the original
+        /// Image instance.
+        /// </summary>
+        /// <param name="image">Image instance</param>
+        /// <param name="width">desired width</param>
+        /// <param name="height">desired height</param>
+        /// <returns>new resized/rotated Image instance</returns>
+        public static Image Resize(Image image, int width, int height)
+        {
+            // clone the Image instance, since we don't want to resize the original Image instance
+            var rotatedImage = image.Clone() as Image;
+            var newSize = CalculateResizedDimensions(rotatedImage, width, height);
+
+            var resizedImage = new Bitmap(newSize.Width, newSize.Height, PixelFormat.Format32bppArgb);
+            resizedImage.SetResolution(72, 72);
+
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                // set parameters to create a high-quality thumbnail
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var attribute = new ImageAttributes())
+                {
+                    attribute.SetWrapMode(WrapMode.TileFlipXY);
+
+                    // draws the resized image to the bitmap
+                    graphics.DrawImage(rotatedImage, new Rectangle(new Point(0, 0), newSize), 0, 0, rotatedImage.Width, rotatedImage.Height, GraphicsUnit.Pixel, attribute);
+                }
+            }
+
+            return resizedImage;
+        }
+
+        public static Image ReduceImageQuality(Image image)
+        {
+            // clone the Image instance, since we don't want to resize the original Image instance
+            var rotatedImage = image.Clone() as Image;
+            var newSize = new Size(rotatedImage.Width, rotatedImage.Height);
+            var resizedImage = new Bitmap(newSize.Width, newSize.Height, PixelFormat.Format24bppRgb);
+            resizedImage.SetResolution(72, 72);
+
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                // set parameters to create a high-quality thumbnail
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var attribute = new ImageAttributes())
+                {
+                    attribute.SetWrapMode(WrapMode.TileFlipXY);
+
+                    // draws the resized image to the bitmap
+                    graphics.DrawImage(rotatedImage, new Rectangle(new Point(0, 0), newSize), 0, 0, rotatedImage.Width, rotatedImage.Height, GraphicsUnit.Pixel, attribute);
+                }
+            }
+
+            return resizedImage;
+        }
+
+        public static Image ResizeThumbnail(Image image)
+        {
+            return Resize(image, ALBUM_PHOTO_THUMB_WIDTH, ALBUM_PHOTO_THUMB_HEIGH);
+        }
+
+        /// <summary>
+        /// Calculates resized dimensions for an image, preserving the aspect ratio.
+        /// </summary>
+        /// <param name="image">Image instance</param>
+        /// <param name="desiredWidth">desired width</param>
+        /// <param name="desiredHeight">desired height</param>
+        /// <returns>Size instance with the resized dimensions</returns>
+        private static Size CalculateResizedDimensions(Image image, int desiredWidth, int desiredHeight)
+        {
+            var widthScale = (double)desiredWidth / image.Width;
+            var heightScale = (double)desiredHeight / image.Height;
+
+            // scale to whichever ratio is smaller, this works for both scaling up and scaling down
+            var scale = widthScale < heightScale ? widthScale : heightScale;
+
+            return new Size
+            {
+                Width = (int)(scale * image.Width),
+                Height = (int)(scale * image.Height)
+            };
+        }
+    }
+}
