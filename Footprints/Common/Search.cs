fca8d33eb0a9db3b91ec48b81020ec8d8b5815f2
thangpc <pcthang1992@gmail.com>
2014-08-15 18:03:26 +0700

    Search

--- Footprints/Common/Search.cs
+++ Footprints/Common/Search.cs
@@ -11,40 +11,46 @@ namespace Footprints.Common
     public class Search : RepositoryBase<Search>, ISearch
     {
         public Search(IGraphClient client) : base(client) { }
-        public IList<User> SearchUser(String textEntered)
+
+        public IList<Object> GeneralSearch(String TextEntered)
+        {
+
+            return null;
+        }
+        public IList<User> SearchUser(String TextEntered)
         {            
             return Db.Cypher.Start(new
             {
-                UserName = Node.ByIndexQuery("node_auto_index", "UserName:\"" + textEntered + "\""),
-                FirstName = Node.ByIndexQuery("node_auto_index", "FirstName:\"" + textEntered + "\""),
-                LastName = Node.ByIndexQuery("node_auto_index", "LastName:\"" + textEntered + "\""),
-                Email = Node.ByIndexQuery("node_auto_index", "Email:\"" + textEntered + "\"")
+                UserName = Node.ByIndexQuery("node_auto_index", "UserName:\"" + TextEntered + "\""),
+                FirstName = Node.ByIndexQuery("node_auto_index", "FirstName:\"" + TextEntered + "\""),
+                LastName = Node.ByIndexQuery("node_auto_index", "LastName:\"" + TextEntered + "\""),
+                Email = Node.ByIndexQuery("node_auto_index", "Email:\"" + TextEntered + "\"")
             }).Match("(UserName:User)").Match("(FirstName:User)").Match("(LastName:User)").Match("(Email:User)").
                 Return<User>("UserName, FirstName, LastName, Email").Results.ToList<User>();
         }
-        public IList<Journey> SearchJourney(String textEntered)
+        public IList<Journey> SearchJourney(String TextEntered)
         {
             return Db.Cypher.Start(new
             {
-                Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + textEntered + "\""),
-                Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + textEntered + "\"")
+                Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + TextEntered + "\""),
+                Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + TextEntered + "\"")
             }).Match("(Name:Journey)").Match("(Description:Journey)").
             Return<Journey>("Name, Description").Results.ToList<Journey>();
         }
-        public IList<Destination> SearchDestination(String textEntered)
+        public IList<Destination> SearchDestination(String TextEntered)
         {
             return Db.Cypher.Start(new
             {
-                Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + textEntered + "\""),
-                Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + textEntered + "\"")
+                Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + TextEntered + "\""),
+                Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + TextEntered + "\"")
             }).Match("(Name:Destination)").Match("(Description:Destination)").
             Return<Destination>("Name, Description").Results.ToList<Destination>();
         }
     }
     public interface ISearch
     {
-        IList<User> SearchUser(String textEntered);
-        IList<Journey> SearchJourney(String textEntered);
-        IList<Destination> SearchDestination(String textEntered);
+        IList<User> SearchUser(String TextEntered);
+        IList<Journey> SearchJourney(String TextEntered);
+        IList<Destination> SearchDestination(String TextEntered);
     }
 }
