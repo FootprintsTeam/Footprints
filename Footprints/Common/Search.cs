aa4e928f384779b71fffb83e9127cec423fdecb5
thangpc <pcthang1992@gmail.com>
2014-08-17 14:01:19 +0700

    Search

--- Footprints/Common/Search.cs
+++ Footprints/Common/Search.cs
@@ -18,33 +18,74 @@ namespace Footprints.Common
             return null;
         }
         public IList<User> SearchUser(String TextEntered)
-        {            
-            return Db.Cypher.Start(new
+        {
+            var query = Db.Cypher.Start(new
             {
                 UserName = Node.ByIndexQuery("node_auto_index", "UserName:\"" + TextEntered + "\""),
                 FirstName = Node.ByIndexQuery("node_auto_index", "FirstName:\"" + TextEntered + "\""),
                 LastName = Node.ByIndexQuery("node_auto_index", "LastName:\"" + TextEntered + "\""),
                 Email = Node.ByIndexQuery("node_auto_index", "Email:\"" + TextEntered + "\"")
-            }).Match("(UserName:User)").Match("(FirstName:User)").Match("(LastName:User)").Match("(Email:User)").
-                Return<User>("UserName, FirstName, LastName, Email").Results.ToList<User>();
+            }).
+            Match("(UserName:User)").Match("(FirstName:User)").Match("(LastName:User)").Match("(Email:User)").
+            Return((UserName, FirstName, LastName, Email) => new
+             {
+                 UserName = UserName.As<User>(),
+                 FirstName = FirstName.As<User>(),
+                 LastName = LastName.As<User>(),
+                 Email = Email.As<User>()
+             }).
+            Results;
+            List<User> result = new List<User>();
+            foreach (var item in query)
+            {
+                if (item.UserName != null) result.Add(item.UserName);
+                if (item.FirstName != null) result.Add(item.FirstName);
+                if (item.LastName != null) result.Add(item.LastName);
+                if (item.Email != null) result.Add(item.Email);
+            }
+            return query.Count() == 0 ? null : result;
         }
         public IList<Journey> SearchJourney(String TextEntered)
         {
-            return Db.Cypher.Start(new
+            var query = Db.Cypher.Start(new
             {
                 Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + TextEntered + "\""),
                 Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + TextEntered + "\"")
             }).Match("(Name:Journey)").Match("(Description:Journey)").
-            Return<Journey>("Name, Description").Results.ToList<Journey>();
+            Return((Name, Description) => new 
+            {
+                Name = Name.As<Journey>(),
+                Description = Name.As<Journey>()
+            }).
+            Results;
+            List<Journey> result = new List<Journey>();
+            foreach (var item in query)
+            {
+                if (item.Name != null) result.Add(item.Name);
+                if (item.Description != null) result.Add(item.Description);
+            }
+            return query.Count() == 0 ? null : result;
+
         }
         public IList<Destination> SearchDestination(String TextEntered)
         {
-            return Db.Cypher.Start(new
+            var query = Db.Cypher.Start(new
             {
                 Name = Node.ByIndexQuery("node_auto_index", "Name:\"" + TextEntered + "\""),
                 Description = Node.ByIndexQuery("node_auto_index", "Description:\"" + TextEntered + "\"")
             }).Match("(Name:Destination)").Match("(Description:Destination)").
-            Return<Destination>("Name, Description").Results.ToList<Destination>();
+            Return((Name, Description) => new
+            {
+                Name = Name.As<Destination>(),
+                Description = Description.As<Destination>()
+            }).Results;
+            List<Destination> result = new List<Destination>();
+            foreach (var item in query)
+            {
+                if (item.Name != null) result.Add(item.Name);
+                if (item.Description != null) result.Add(item.Description);
+            }
+            return query.Count() == 0 ? null : result;
         }
     }
     public interface ISearch
