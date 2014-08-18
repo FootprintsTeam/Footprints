6ab6446d85705e338bb39f3ac5d09d1a7229cc8e
njen <tantrokiniem@gmail.com>
2014-08-19 00:14:25 +0700

    Test

---
+++ Footprints.Tests/DAL/Concrete/CommentRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Footprints.DAL.Concrete;
+using NUnit.Framework;
+using Footprints.Models;
+using Neo4jClient;
+namespace Footprints.DAL.Concrete.Tests
+{
+    [TestFixture()]
+    public class CommentRepositoryTests
+    {
+        IGraphClient client = new GraphClient(new Uri("http://54.179.157.145:7474/db/data"));
+        CommentRepository commentRepository;
+        [Test()]
+        public void GetAllCommentOnJourneyTest()
+        {
+            Guid JourneyID = new Guid("dcd7f7ca-5d4d-41ad-9c8a-172f395062ac");
+            IList<Comment> result = commentRepository.GetAllCommentOnJourney(JourneyID);
+            Assert.AreEqual(result.Count, 1);
+        }
+    }
+}
