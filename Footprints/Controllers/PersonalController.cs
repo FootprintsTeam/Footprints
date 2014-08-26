c74dfa05085bb4199458a697c727f81996913020
nhanloukiala <tantrokiniem@gmail.com>
2014-08-26 16:56:59 +0700

    finalize personal timeline

--- Footprints/Controllers/PersonalController.cs
+++ Footprints/Controllers/PersonalController.cs
@@ -14,6 +14,7 @@ using Footprints.Services;
 using AutoMapper;
 using Footprints.Common.JsonModel;
 using Footprints.Common;
+using System.IO;
 
 namespace Footprints.Controllers
 {
@@ -107,6 +108,44 @@ namespace Footprints.Controllers
             };
 
             return viewModels;
+        }
+
+        [HttpPost]
+        public ActionResult InfiniteScroll(int BlockNumber)
+        {
+            ////////////////// THis line of code only for demo. Needs to be removed ////
+            System.Threading.Thread.Sleep(1000);
+            ////////////////////////////////////////////////////////////////////////////
+            //int BlockSize = 5;
+            //var books = DataManager.GetBooks(BlockNumber, BlockSize);
+            IList<InfiniteScrollJsonModel> jsonModels = new List<InfiniteScrollJsonModel>();
+            //jsonModel.NoMoreData = books.Count < BlockSize;
+            jsonModels.Add(new InfiniteScrollJsonModel { HTMLString = RenderPartialViewToString("CommentWidget", CommentWidgetViewModel.GetSampleObject()) });
+            jsonModels.Add(new InfiniteScrollJsonModel { HTMLString = RenderPartialViewToString("DestinationWidget", DestinationWidgetViewModel.GetSampleObject()) });
+            jsonModels.Add(new InfiniteScrollJsonModel { HTMLString = RenderPartialViewToString("AddFriendWidget", AddFriendWidgetViewmodel.GetSampleObject()) });
+            jsonModels.Add(new InfiniteScrollJsonModel { HTMLString = RenderPartialViewToString("JourneyWidget", JourneyWidgetViewModel.GetSampleObject()) });
+            jsonModels.Add(new InfiniteScrollJsonModel { HTMLString = RenderPartialViewToString("ShareWidget", ShareWidgetViewModel.GetSampleObject()) });
+
+            return Json(jsonModels);
+        }
+
+        protected string RenderPartialViewToString(string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+                viewName = ControllerContext.RouteData.GetRequiredString("action");
+
+            ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult =
+                ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext
+                (ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
         }
 
         public ActionResult Index(string userID = "default")
--- Footprints/Helpers/HtmlBuilder.cs
+++ Footprints/Helpers/HtmlBuilder.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Footprints.ViewModels;
+using Footprints.Common;
 using System.IO;
 
 namespace Footprints.Helpers
 {
     public static class HtmlBuilder
     {
-        
+        public static string GetPersonalPartialViewName(this NewsfeedBaseWidgetViewModel activity) {
+            switch (activity.Type)
+            {
+                case Constant.ActivityAddNewContent:
+                    return "PersonalAddPhotoWidget";                    
+                    
+
+                case Constant.ActivityAddNewDestination:
+                    return "PersonalDestinationWidget";                    
+                    
+
+                case Constant.ActivityAddNewFriend:
+                    //Html.RenderPartial("AddFriendWidget", activity);
+                    break;
+
+                case Constant.ActivityAddnewJourney:
+                    return "PersonalJourneyWidget";                    
+                    
+
+                case Constant.ActivityComment:
+                    return "PersonalCommentWidget";                    
+                    
+
+                case Constant.ActivityShareDestination:
+                    return "PersonalShareWidget";                    
+                default:
+                    System.Diagnostics.Debug.WriteLine(activity.Type + "something");
+                    return "false";                    
+            }
+            return "false";
+        }
     }
 }
