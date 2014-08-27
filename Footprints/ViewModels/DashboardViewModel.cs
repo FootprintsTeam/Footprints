a7099a4d6acb95c14180e142a538d4bd97b96ded
njen <tantrokiniem@gmail.com>
2014-08-27 17:07:34 +0700

    Fix

---
+++ Footprints/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Footprints.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TodayRegisterUser { get; set; }
+        public int TotalUser { get; set; }
+        public int TodayNewJourney { get; set; }
+        public int TotalJourney { get; set; }
+        public int TodayNewDestination { get; set; }
+        public int TotalDestination { get; set; }
+    }
+}
